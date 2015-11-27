b1b5dfcc9386d16a2c70f105949adae37b41cc55
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2015-11-27 15:36:49 +0000

    Removed uneccessary logback.groovy file from phenodigm2-graph test/resources

--- phenodigm2-graph/src/test/resources/logback.groovy
+++
@@ -1,9 +0,0 @@
-appender("CONSOLE", ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] %-5level %logger - %msg%n"
-    }
-}
-
-logger("uk.ac.sanger.phenodigm2", INFO)
-
-root(INFO, ["CONSOLE"])
